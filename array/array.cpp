8dc966f828b9835e738feb24d85d2a15c33c16c4
Aymen Ghazali <ayghazal@e1r10p10.1337.ma>
2021-11-24 21:34:37 +0100

    yallah

---
+++ array/array.cpp
@@ -0,0 +1,8 @@
+#include "array.hpp"
+
+int     main()
+{
+    ft::Array<int, 10> ar;
+    std::cout << ar.size() << std::endl;
+
+}
--- array/array.hpp
+++ array/array.hpp
@@ -3,8 +3,19 @@
 
 #include <iostream>
 
- 
+namespace ft
+{
+	template <typename T, size_t len>
+	class Array
+	{
+		private :
+		T     _data[len];
 
-
+		public :
+		int	size() const{return len;}
+		T &operator[](int index) {return _data[index];}
+	
+	};
+}
 
 #endif
