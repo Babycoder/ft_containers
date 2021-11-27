a9201f7354d3cdab150d66eb6b0db02d1faa62de
Aymen Ghazali <ayghazal@e2r9p11.1337.ma>
2021-11-27 18:55:12 +0100

    letgo

--- iterators.hpp
+++ iterators.hpp
@@ -4,16 +4,18 @@
 
 #include <iostream>
 
+namespace ft {
 
-template <typename T>
-class iterator
-{
-	typedef		T& reference; 
-	typedef		T* pointer;
-	typedef 	ptrdiff_t difference_type;
-	typedef		T value_type;
+	template <typename T>
+	class iterator
+	{
+		public :
+			typedef		T& reference; 
+			typedef		T* pointer;
+			typedef 	std::ptrdiff_t difference_type;
+			typedef		T value_type;
 
-	
-};
+	};
+}
 
 #endif
