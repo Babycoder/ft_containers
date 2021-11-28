58067ec0d124b56da8700a2418ca2be74463c5c3
Aymen Ghazali <ayghazal@e2r9p11.1337.ma>
2021-11-28 11:10:15 +0100

    operators

--- iterators.hpp
+++ iterators.hpp
@@ -10,10 +10,29 @@ namespace ft {
 	class iterator
 	{
 		public :
+			
 			typedef		T& reference; 
 			typedef		T* pointer;
 			typedef 	std::ptrdiff_t difference_type;
 			typedef		T value_type;
+
+			iterator(pointer ptr) : _ptr(ptr){};
+
+			reference operator*() const { return *_ptr; };
+			pointer   operator->() { return _ptr; };
+			
+			iterator& operator++() { _ptr++; return *this };
+			iterator operator++(T) { iterator tmp = *this; ++(*this); return tmp; };
+
+			friend bool operator==(const iterator& a, const iterator& b) { return a._ptr == b._ptr; };
+			friend bool operator!=(const iterator& a, const iterator& b) { return a._ptr != b._ptr; };
+
+
+
+
+
+		private :
+			pointer _ptr;
 
 	};
 }
