3482889c7e9e577860f6daf7c94ec8fc563443f5
ghazali <aymenghazali1999@gmail.com>
2022-02-18 12:29:02 +0100

    main work

--- BinaryTree/main.cpp
+++ BinaryTree/main.cpp
@@ -10,9 +10,14 @@ int main()
 
    // ft::AVL<value_type, Compare, Alloc> _avl;
 
-    //ft::AVL< ft::pair<const int, char>, std::less<int>, std::allocator<ft::pair<const int, char>>    _tree;
+    ft::AVL<ft::pair<const int, char>, std::less<int>, std::allocator<ft::pair<const int, char>>>    _tree;
    
-    //_tree.insert({1, 'a'});
+    _tree.insert({1, 'a'});
+    _tree.insert({2, 'b'});
+    _tree.insert({0, 'z'});
+    //_tree.insert({3, 'c'});
+    std::cout << _tree.root->data->first << std::endl;
+   // _tree.display();
     //ft::AVL < {'a', 1}, std::less<char, std::allocator<ft::pair<const char,int>> tree;
 
     
